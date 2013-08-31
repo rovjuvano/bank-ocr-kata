07873207ef793aaf10f79d50891cfe8fc6b81d4a
Robert Juliano <juliano.1@osu.edu>
2013-08-31 15:07:36 -0400

    Switch #digits to use Enumerator

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,13 +1,15 @@
 class BankOCRScanner
   def parse(io)
-    result = ''
-    digits(io) do |d|
-      result << (d =~ /^ _ / ? '0' : '1')
-    end
-    [result]
+    [
+      digits(io).collect() do |d|
+        d =~ /^ _ / ? '0' : '1'
+      end.join('')
+    ]
   end
 
-  def digits(io, &block)
-    9.times { yield io.gets(3) }
+  def digits(io)
+    Enumerator.new do |y|
+      9.times { y << io.gets(3) }
+    end
   end
 end
