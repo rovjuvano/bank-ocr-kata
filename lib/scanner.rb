a8918010189f8147122b424b9fcba2ece449eb81
Robert Juliano <juliano.1@osu.edu>
2013-08-31 14:35:26 -0400

    Add support for mixing digit classes within an entry

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,5 +1,13 @@
 class BankOCRScanner
   def parse(io)
-    io.gets(3) == ' _ ' ? '000000000' : '111111111'
+    result = ''
+    digits(io) do |d|
+      result << (d =~ /^ _ / ? '0' : '1')
+    end
+    result
+  end
+
+  def digits(io, &block)
+    9.times { yield io.gets(3) }
   end
 end
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -30,6 +30,19 @@ describe BankOCRScanner do
         When(:result) { scanner.parse(file) }
         Then { result == '111111111' }
       end
+
+      context 'with mixed digits' do
+        Given(:file) {
+          StringIO.new(
+            " _  _        _        _  _ \n" +
+            "| || |  |  || |  |  || || |\n" +
+            "|_||_|  |  ||_|  |  ||_||_|\n" +
+            "\n"
+          )
+        }
+        When(:result) { scanner.parse(file) }
+        Then { result == '001101100' }
+      end
     end
   end
 end
