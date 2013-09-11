0005fa0ee8fe5bbf6337c316069ea78833f5efaf
Robert Juliano <juliano.1@osu.edu>
2013-09-11 19:04:51 -0400

    [code] Learn to recognize digits '2'-'9'

--- lib/ocr/scanned_digit.rb
+++ lib/ocr/scanned_digit.rb
@@ -1,11 +1,44 @@
 module OCR
   class ScannedDigit
+    TO_DIGIT = {
+      ' _ ' +
+      '| |' +
+      '|_|' => '0',
+      '   ' +
+      '  |' +
+      '  |' => '1',
+      ' _ ' +
+      ' _|' +
+      '|_ ' => '2',
+      ' _ ' +
+      ' _|' +
+      ' _|' => '3',
+      '   ' +
+      '|_|' +
+      '  |' => '4',
+      ' _ ' +
+      '|_ ' +
+      ' _|' => '5',
+      ' _ ' +
+      '|_ ' +
+      '|_|' => '6',
+      ' _ ' +
+      '  |' +
+      '  |' => '7',
+      ' _ ' +
+      '|_|' +
+      '|_|' => '8',
+      ' _ ' +
+      '|_|' +
+      ' _|' => '9',
+    }
+
     def initialize(line1, line2, line3)
       @value_raw = line1 + line2 + line3
     end
 
     def value()
-      @value_raw =~ /^ _ / ? '0' : '1'
+      TO_DIGIT[@value_raw]
     end
   end
 end
