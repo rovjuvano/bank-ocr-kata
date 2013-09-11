56da60e0430a58ac79c776d15040d9def53c2ac2
Robert Juliano <juliano.1@osu.edu>
2013-09-11 18:04:40 -0400

    [code] Learn to recognize digit '1'

--- lib/ocr/scanned_digit.rb
+++ lib/ocr/scanned_digit.rb
@@ -1,10 +1,11 @@
 module OCR
   class ScannedDigit
     def initialize(line1, line2, line3)
+      @value_raw = line1 + line2 + line3
     end
 
     def value()
-      return '0'
+      @value_raw =~ /^ _ / ? '0' : '1'
     end
   end
 end
