e55a1eea7a8c49cb0d5deb4f1f816ae51502e9d2
Robert Juliano <juliano.1@osu.edu>
2013-09-11 18:51:35 -0400

    [clean] Remove duplication from digit recognition tests

--- spec/ocr/scanned_digit_spec.rb
+++ spec/ocr/scanned_digit_spec.rb
@@ -2,16 +2,12 @@ require 'spec_helper'
 
 module OCR
   describe ScannedDigit do
-    context('when pattern represents 0') do
-      Given(:lines) { ocr_digit('0') }
-      When(:digit) { ScannedDigit.new(*lines) }
-      Then { digit.value == '0' }
-    end
-
-    context('when pattern represents 1') do
-      Given(:lines) { ocr_digit('1') }
-      When(:digit) { ScannedDigit.new(*lines) }
-      Then { digit.value == '1' }
+    ('0'..'1').each do |digit|
+      context("when pattern represents #{digit}") do
+        Given(:lines) { ocr_digit(digit) }
+        When(:scanned_digit) { OCR::ScannedDigit.new(*lines) }
+        Then { scanned_digit.value == digit }
+      end
     end
   end
 end
