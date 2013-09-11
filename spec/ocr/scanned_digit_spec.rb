5eb6e06bf2880ff3075182fddfbdfd4e77c8b2a5
Robert Juliano <juliano.1@osu.edu>
2013-09-11 18:10:28 -0400

    [clean] Add spec helper method for generating OCR digits

--- spec/ocr/scanned_digit_spec.rb
+++ spec/ocr/scanned_digit_spec.rb
@@ -3,21 +3,13 @@ require 'spec_helper'
 module OCR
   describe ScannedDigit do
     context('when pattern represents 0') do
-      Given(:lines) {[
-        ' _ ',
-        '| |',
-        '|_|'
-      ]}
+      Given(:lines) { ocr_digit('0') }
       When(:digit) { ScannedDigit.new(*lines) }
       Then { digit.value == '0' }
     end
 
     context('when pattern represents 1') do
-      Given(:lines) {[
-        '   ',
-        '  |',
-        '  |'
-      ]}
+      Given(:lines) { ocr_digit('1') }
       When(:digit) { ScannedDigit.new(*lines) }
       Then { digit.value == '1' }
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 require 'rspec-given'
 require 'ocr'
+
+TO_OCR_DIGIT = {
+  '0' => [ ' _ ',
+           '| |',
+           '|_|' ],
+  '1' => [ '   ',
+           '  |',
+           '  |' ],
+}
+def ocr_digit(number)
+  TO_OCR_DIGIT[number]
+end
