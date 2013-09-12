6fc049e7a239cd7ea871f735e401f55210ef4f69
Robert Juliano <juliano.1@osu.edu>
2013-09-12 17:43:25 -0400

    [code] Learn base validation strategy - always valid

--- lib/ocr.rb
+++ lib/ocr.rb
@@ -1,3 +1,4 @@
 require 'ocr/scanned_digit'
 require 'ocr/scanned_number'
 require 'ocr/ocr_file'
+require 'ocr/validation/base'
---
+++ lib/ocr/validation/base.rb
@@ -0,0 +1,10 @@
+module OCR
+  module Validation
+    class Base
+      def valid?(digits)
+        true
+      end
+    end
+  end
+end
+
