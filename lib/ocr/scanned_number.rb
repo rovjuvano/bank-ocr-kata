6c1471e39acf8edbb7230fa1628b725eb9e94d0a
Robert Juliano <juliano.1@osu.edu>
2013-09-12 12:03:52 -0400

    [doc] TomDoc OCR::ScannedNumber

--- lib/ocr/scanned_number.rb
+++ lib/ocr/scanned_number.rb
@@ -1,14 +1,26 @@
 module OCR
+  # Public: Encapsulate a single number/entry from an OCR file.
   class ScannedNumber
+    # Public: Initialize a single number/entry from the three lines of OCR data.
+    #
+    # line1 - The top line of the digits.
+    # line2 - The middle line of the digits.
+    # line3 - The bottom line of the digits.
     def initialize(line1, line2, line3)
       @lines = [line1, line2, line3]
     end
 
+    # Public: Get the String representation of the number/entry.
+    #
+    # Returns the String representation of the number/entry.
     def value
       digits.collect { |digit| digit.value }.join('')
     end
 
     protected
+      # Internal: Split lines into single digits.
+      #
+      # Returns the Enumerable of ScannedDigits.
       def digits()
         (0..27).step(3).collect do |base|
           ScannedDigit.new(@lines[0][base, 3],
