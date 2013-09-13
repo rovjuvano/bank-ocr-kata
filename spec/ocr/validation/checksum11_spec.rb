d0e5917ba49aa51054068a63acd6984f6e18b7fa
Robert Juliano <juliano.1@osu.edu>
2013-09-13 16:31:07 -0400

    [clean] Eliminate duplication from test setups

--- spec/ocr/validation/checksum11_spec.rb
+++ spec/ocr/validation/checksum11_spec.rb
@@ -12,17 +12,18 @@ module OCR
   module Validation
     describe Checksum11 do
       Given(:validator) { Checksum11.new }
+      Given(:digits) { make_digits(number) }
 
       describe '#checksum' do
         context 'zeros have no effect' do
-          Given(:digits) { make_digits('000000000') }
+          Given(:number) { '000000000' }
           Then { validator.checksum(digits) == 0 }
         end
 
         context 'digit is multiplied by distance from end, then mods by 11' do
           (1..9).each do |i|
             context "with digit #{i} from end" do
-              Given(:digits) { make_digits('00000000'.insert(-i, '7')) }
+              Given(:number) { '00000000'.insert(-i, '7') }
               Then { validator.checksum(digits) == 7*i % 11 }
             end
           end
@@ -31,27 +32,27 @@ module OCR
         context 'after multiplying, values are summed' do
           (2..9).each do |i|
             context "digits 1 and #{i} from the end" do
-              Given(:digits) { make_digits('00000001'.insert(-i, '1')) }
+              Given(:number) { '00000001'.insert(-i, '1') }
               Then { validator.checksum(digits) == i+1 }
             end
           end
         end
 
         context 'after summation, value is modded by 11' do
-          Given(:digits) { make_digits('100000002') }
+          Given(:number) { '100000002' }
           Then { validator.checksum(digits) == 0 }
         end
       end
 
       describe '#valid?' do
         context 'with checksum == 0' do
-          Given(:digits) { make_digits('100000002') }
+          Given(:number) { '100000002' }
           Then { validator.valid?(digits) }
         end
 
         (1..10).each do |n|
           context "with checksum == #{n}" do
-            Given(:digits) { make_digits('010001000' + n.to_s) }
+            Given(:number) { '010001000' + n.to_s }
             Then { not validator.valid?(digits) }
           end
         end
