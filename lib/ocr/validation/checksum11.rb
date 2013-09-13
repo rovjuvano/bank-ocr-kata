3c0dace320837904a2b29cae6e272b79ff29444e
Robert Juliano <juliano.1@osu.edu>
2013-09-13 15:06:17 -0400

    [code] Checksum11 learns to multiply each digit by distance from the end

--- lib/ocr/validation/checksum11.rb
+++ lib/ocr/validation/checksum11.rb
@@ -2,6 +2,9 @@ module OCR
   module Validation
     class Checksum11 < Base
       def checksum(digits)
+        digits.each_with_index do |digit, i|
+          return digit.value.to_i * (9-i) if digit.value.to_i > 0
+        end
         0
       end
     end
