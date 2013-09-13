5a621044831f74f653fb1c50c01a1728460318ca
Robert Juliano <juliano.1@osu.edu>
2013-09-13 16:57:32 -0400

    [doc] TomDoc Validation classes

--- lib/ocr/validation/base.rb
+++ lib/ocr/validation/base.rb
@@ -1,6 +1,12 @@
 module OCR
   module Validation
+    # Public: Base class for validating numbers.
     class Base
+      # Public: Indicate whether number is among the valid set of account numbers.
+      #
+      # digits - The Enumerator of the nine ScannedDigits of the number.
+      #
+      # Returns true, always.
       def valid?(digits)
         true
       end
--- lib/ocr/validation/checksum11.rb
+++ lib/ocr/validation/checksum11.rb
@@ -1,10 +1,19 @@
 module OCR
   module Validation
+    # Public: A checkum based validation strategy.
     class Checksum11 < Base
+      # Override
       def valid?(digits)
         checksum(digits) == 0
       end
 
+      # Internal: Calculate checksum used in validation.
+      #
+      # digits - see #valid?
+      #
+      # SUM(digits[i] * (9 - i)) % 11 for i in 0 to 8
+      #
+      # Returns the Integer checksum.
       def checksum(digits)
         digits.each_with_index.inject(0) do |sum, (digit, i)|
           sum + digit.value.to_i * (9-i)
