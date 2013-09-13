b6c9fa948f7aca5c06aa6975539959c791b6c8e8
Robert Juliano <juliano.1@osu.edu>
2013-09-13 15:59:25 -0400

    [code] Checksum11 learns to invalidate numbers with non-zero checksum

--- lib/ocr/validation/checksum11.rb
+++ lib/ocr/validation/checksum11.rb
@@ -1,6 +1,10 @@
 module OCR
   module Validation
     class Checksum11 < Base
+      def valid?(digits)
+        false
+      end
+
       def checksum(digits)
         digits.each_with_index.inject(0) do |sum, (digit, i)|
           sum + digit.value.to_i * (9-i)
