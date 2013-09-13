b35ae72d1bfdeb9c6ba46c9edff7112ab33448a4
Robert Juliano <juliano.1@osu.edu>
2013-09-13 15:02:27 -0400

    [test] Require Checksum11 to multiply each digit by distance from the end

--- spec/ocr/validation/checksum11_spec.rb
+++ spec/ocr/validation/checksum11_spec.rb
@@ -20,6 +20,15 @@ module OCR
         Given(:digits) { make_digits('000000000') }
         Then { validator.checksum(digits) == 0 }
       end
+
+      context 'digit is multiplied by distance from end' do
+        (1..9).each do |i|
+          context "with digit #{i} from end" do
+            Given(:digits) { make_digits('00000000'.insert(-i, '7')) }
+            Then { validator.checksum(digits) == 7*i }
+          end
+        end
+      end
     end
   end
 end
