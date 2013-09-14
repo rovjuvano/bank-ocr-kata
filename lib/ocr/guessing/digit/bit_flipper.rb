6572e8f6d00a613600647ed5e348520974b4077a
Robert Juliano <juliano.1@osu.edu>
2013-09-14 17:17:39 -0400

    [code] BitFlipper Digit Guesser learns to have one guess when there is a single match

--- lib/ocr/guessing/digit/bit_flipper.rb
+++ lib/ocr/guessing/digit/bit_flipper.rb
@@ -2,6 +2,14 @@ module OCR
   module Guessing
     module Digit
       class BitFlipper < Base
+        def guesses(line1, line2, line3)
+          a = [line1, line2, line3]
+          line = OCR::ScannedDigit::TO_DIGIT.keys.find do |b|
+            differences(a, b.scan(/.../)) == 1
+          end
+          line ? [ OCR::ScannedDigit.new(*line.scan(/.../)) ] : []
+        end
+
         def differences(a, b)
           as = a.join('')
           bs = b.join('')
