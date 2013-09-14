fbc2502fdf7810350c5ae886f70018ccea3eea8a
Robert Juliano <juliano.1@osu.edu>
2013-09-14 15:55:56 -0400

    [test] Require BitFlipper Digit Guesser to calculate difference between number and guess with one difference

--- spec/ocr/guessing/digit/bit_flipper_spec.rb
+++ spec/ocr/guessing/digit/bit_flipper_spec.rb
@@ -7,9 +7,15 @@ module OCR
         Given(:guesser) { BitFlipper.new }
 
         describe '#differences' do
+          Given(:a) { ['   ', '   ', '   '] }
+
           context 'with 0 differences' do
-            Given(:a) { ['   ', '   ', '   '] }
             Then { guesser.differences(a, a) == 0 }
+          end
+
+          context 'with 1 difference' do
+            Given(:b) { ['  |', '   ', '   '] }
+            Then { guesser.differences(a, b) == 1 }
           end
         end
       end
