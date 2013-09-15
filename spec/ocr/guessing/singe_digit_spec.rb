3cbc653529cd513cbc1ef94b5c5dc69b0b3cd035
Robert Juliano <juliano.1@osu.edu>
2013-09-15 18:04:58 -0400

    [test] Require Single Digit Guesser to have multiple guesses with multiple matches

--- spec/ocr/guessing/singe_digit_spec.rb
+++ spec/ocr/guessing/singe_digit_spec.rb
@@ -6,7 +6,7 @@ module OCR
       Given(:guesser) { SingleDigit.new }
       Given(:scanned_number) { ocr_scanned_number(number) }
       When(:guesses) { guesser.guesses(scanned_number) }
-      Invariant { guesses == expected }
+      Invariant { guesses.sort == expected.sort }
 
       context 'with no matches' do
         Given(:number) { :underscores }
@@ -17,6 +17,12 @@ module OCR
       context 'with a single match' do
         Given(:number) { :ones }
         Given(:expected) { ['711111111'] }
+        Then {}
+      end
+
+      context 'with multiple matches' do
+        Given(:number) { :eights }
+        Given(:expected) { ['888886888', '888888880', '888888988'] }
         Then {}
       end
     end
