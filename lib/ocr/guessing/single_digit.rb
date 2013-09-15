705008052b09afab497da1f9b09e2cd823afff56
Robert Juliano <juliano.1@osu.edu>
2013-09-15 18:59:15 -0400

    [code] Single Digit Guesser learns to have multiple guesses with multiple matches

--- lib/ocr/guessing/single_digit.rb
+++ lib/ocr/guessing/single_digit.rb
@@ -2,8 +2,27 @@ module OCR
   module Guessing
     class SingleDigit < Base
       def guesses(number)
-        number.value =~ /^1/ ? ['711111111'] : []
+        source_digits = number.digits.to_a
+        guessed_digits = []
+        guesses = []
+
+        source_digits.each_with_index do |digit, i|
+          digit.guesses.each do |guessed_digit|
+            guessed_digits.replace(source_digits)
+            guessed_digits[i] = guessed_digit
+            if validator.valid?(guessed_digits)
+              guesses << guessed_digits.collect { |d| d.value }.join('')
+            end
+          end
+        end
+
+        guesses
       end
+
+      protected
+        def validator
+          (@@validator ||= Validation::Checksum11.new)
+        end
     end
   end
 end
